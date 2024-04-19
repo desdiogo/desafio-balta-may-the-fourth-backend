24b533e7e17f85de99910a1eed1f2906d711f9aa
Diogo Eleutero <desdiogo10@yahoo.com>
2024-04-19 17:30:00 -0300

    Add Db context

---
+++ MayTheFourth.Infrastructure/MayTheFourthDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MayTheFourth.Infrastructure;
+
+public class MayTheFourthDbContext: DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=127.0.0.1:5432;Database=MayTheFourth;Username=docker;Password=docker");;
+    }
+}
