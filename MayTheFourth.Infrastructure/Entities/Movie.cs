58aee84a7187f5c0ce1e0a50ee750ee96e30d3be
Diogo Eleutero <desdiogo10@yahoo.com>
2024-04-19 18:51:02 -0300

    Add Movie Entity

---
+++ MayTheFourth.Infrastructure/Entities/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace MayTheFourth.Infrastructure.Entities;
+
+public class Movie
+{
+    public ushort Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public ushort Episode { get; set; }
+    public string OpeningCrawl { get; set; } = string.Empty;
+    public string Director { get; set; } = string.Empty;
+    public string Producer { get; set; } = string.Empty;
+    public DateOnly ReleaseDate { get; set; }
+}
