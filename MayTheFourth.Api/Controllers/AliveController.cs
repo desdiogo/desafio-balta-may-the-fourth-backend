0140464b2f05c319d04f8ca7972d7df45a4dec1a
Diogo Eleutero <desdiogo10@yahoo.com>
2024-04-23 09:09:06 -0300

    add alive controller

---
+++ MayTheFourth.Api/Controllers/AliveController.cs
@@ -0,0 +1,19 @@
+﻿using MayTheFourth.Communication.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MayTheFourth.Api.Controllers;
+
+[Route("api[controller]")]
+public class AliveController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Alive()
+    {
+        var response = new ResponseAliveJson
+        {
+            Message = "Is alive"
+        };
+        
+        return Ok(response);
+    }
+}
---
+++ MayTheFourth.Communication/Responses/ResponseAliveJson.cs
@@ -0,0 +1,6 @@
+﻿namespace MayTheFourth.Communication.Responses;
+
+public class ResponseAliveJson
+{
+    public string Message { get; set; } = string.Empty;
+}
