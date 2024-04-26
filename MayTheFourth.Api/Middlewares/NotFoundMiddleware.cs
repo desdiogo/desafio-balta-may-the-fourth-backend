f1d70d4279b5dbeb6fa50285367bea2ff931674c
Diogo Eleutero <desdiogo10@yahoo.com>
2024-04-26 16:55:19 -0300

    fix middleware

--- MayTheFourth.Api/Middlewares/NotFoundMiddleware.cs
+++ MayTheFourth.Api/Middlewares/NotFoundMiddleware.cs
@@ -1,6 +1,6 @@
 ﻿using MayTheFourth.Communication.Responses;
 using MayTheFourth.Enum;
-using Newtonsoft.Json;
+using System.Text.Json;
 
 namespace MayTheFourth.Api.Middlewares;
 
@@ -8,15 +8,19 @@ public class NotFoundMiddleware(RequestDelegate next)
 {
     public async Task Invoke(HttpContext context)
     {
-        
         await next(context);
-        
+
         if (context.Response is { StatusCode: StatusCodes.Status404NotFound, HasStarted: false })
         {
             context.Response.ContentType = ResponseContentType.ApplicationJson;
 
             var response = new ResponseErrorJson(ResponseMessage.NotFound);
-            var jsonResponse = JsonConvert.SerializeObject(response);
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            var jsonResponse = JsonSerializer.Serialize(response, options);
 
             await context.Response.WriteAsync(jsonResponse);
         }
